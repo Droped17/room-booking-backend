b73da76c7936e9791f6ae736359883a50110586a
Champ <droped17@gmail.com>
2023-12-25 16:30:20 +0700

    create project and create server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 8000;
+server.listen(port,()=> console.log(`Server is running in port`,port));
